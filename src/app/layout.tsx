fc521e1b33bec10339bbba9a86709733798e536f
divinejoshua <divineuche399@gmail.com>
2024-02-17 01:39:47 +0000

    Added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+
+      {/* Google tag (gtag.js) */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q5PCGTM9G6"
+        strategy="afterInteractive"
+        async
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-Q5PCGTM9G6');
+        `}
+      </Script>
       <body className={inter.className}>{children}</body>
     </html>
   );
